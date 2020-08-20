786d6beb735f3d7244f9b7633f36da25a921bac0
Eric Ordonez <jwustudent@gmail.com>
2020-08-19 23:46:51 -0500

    Added some comments to the code

--- src/main/java/com/SpringbootTraining/WebsiteUsers/WebsiteUsersApplication.java
+++ src/main/java/com/SpringbootTraining/WebsiteUsers/WebsiteUsersApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.SpringbootTraining.WebsiteUsers.repository")
+
+
+@EnableJpaRepositories(basePackages = "com.SpringbootTraining.WebsiteUsers.repository") //specifying the packages to scan for annotated components
 @SpringBootApplication
 public class WebsiteUsersApplication {
 
